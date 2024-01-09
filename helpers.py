1a68b98cf355d55b4c5890b5cb38b156fdadc554
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-01-09 17:21:08 +0000

    newlist

--- helpers.py
+++ helpers.py
@@ -52,11 +52,11 @@ def reformat_add_df_context(df, facility, submission_id):
     df['submission_id'] = submission_id
     df['Facility'] = df['A']
     df['Facility'] = facility
-    df = df['submission_id','Facility','A','B','C','D','E','F','G','H','I','J','K','L','M','N']
-    # col_order = df.columns.tolist()
-    # new_cols = ['submission_id', 'Facility']
-    # new_col_order = new_cols.extend(col_order)
-    # df = df[new_col_order]
+    # df = df['submission_id','Facility','A','B','C','D','E','F','G','H','I','J','K','L','M','N']
+    col_order = df.columns.tolist()
+    new_cols = ['submission_id', 'Facility']
+    new_col_order = new_cols.extend(col_order)
+    df = df[[new_col_order]]
     return df
 
 
