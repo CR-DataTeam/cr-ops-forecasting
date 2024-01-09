1865fb92e98b11555a1cc4d5a3bedc4771d373f4
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-01-09 18:56:50 +0000

    test

--- Hello.py
+++ Hello.py
@@ -77,7 +77,7 @@ if submitted:
         # new = h.reformat_add_df_context(df,'Ballantyne','asdfasdfasdf')
         # h.stored_APPEND_data(h.ssid_full,'Mamm!A:P',h.excel_storage_conversion(new))
         upfileid = h.upload_file_to_drive(uploaded_file, 'form_test.xlsx')
-        iteration_calc = h.stored_GET_data(h.ssid_subm, 'Mamm!A:K')
+        iteration_calc = h.stored_GET_data(h.ssid_subm, 'Mamm!A2:K')
         asdf = {'ServiceLine':servline_select, 
                 'Year':2024,
                 'Version':forecast_select,
