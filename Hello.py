8e2182678ea9f3728dda54e3219a2403f47c235c
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-01-09 19:01:35 +0000

    test

--- Hello.py
+++ Hello.py
@@ -65,7 +65,7 @@ fl = ['Ballantyne', 'Blakeney', 'Huntersville', 'Matthews',
         'Steele Creek', 'Union West', 'University']
 
 
-iteration_calc = h.stored_GET_data(h.ssid_subm, 'Mamm!A2:K')
+iteration_calc = h.stored_GET_data(h.ssid_subm, 'Mamm!A2:K2')
 iteration_calc
 
 if submitted:
@@ -81,7 +81,7 @@ if submitted:
         # new = h.reformat_add_df_context(df,'Ballantyne','asdfasdfasdf')
         # h.stored_APPEND_data(h.ssid_full,'Mamm!A:P',h.excel_storage_conversion(new))
         upfileid = h.upload_file_to_drive(uploaded_file, 'form_test.xlsx')
-        # iteration_calc = h.stored_GET_data(h.ssid_subm, 'Mamm!A2:K')
+        # iteration_calc = h.stored_GET_data(h.ssid_subm, 'Mamm!A2:K2')
         asdf = {'ServiceLine':servline_select, 
                 'Year':2024,
                 'Version':forecast_select,
