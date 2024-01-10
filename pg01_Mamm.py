452febbb05da142b0ce65bc895269691306bef80
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-01-10 20:24:29 +0000

    test

---
+++ pg01_Mamm.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit.logger import get_logger
+import helpers as h
+import pandas as pd
+from tempfile import NamedTemporaryFile
+
+LOGGER = get_logger(__name__)
+
+# Optional -- adds the title and icon to the current page
+st.set_page_config(
+     page_title="Mamm History",
+     layout="wide"
+     )
+
+forecast_list = ['Forecast Version','00+12','01+11','02+10','03+09','04+08','05+07','06+06','07+05','08+04','09+03','10+02','11+01']
+forecast_select = st.selectbox('Forecast Version', forecast_list, index=1)
+
+df = h.get_iteration(service_line='Mamm', forecast_month=forecast_select)
