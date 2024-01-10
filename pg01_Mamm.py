98a0b19f5b90171f0e93dfad414b79cbd4dbf216
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-01-10 20:26:50 +0000

    test

--- pg01_Mamm.py
+++ pg01_Mamm.py
@@ -15,5 +15,6 @@ st.set_page_config(
 forecast_list = ['Forecast Version','00+12','01+11','02+10','03+09','04+08','05+07','06+06','07+05','08+04','09+03','10+02','11+01']
 forecast_select = st.selectbox('Forecast Version', forecast_list, index=1)
 
-df = h.get_iteration(service_line='Mamm', forecast_month=forecast_select)
+subm_df = h.stored_GET_data(h.ssid_subm, 'All!A1:K')[0]
+df = subm_df[(subm_df['ServiceLine']=='Mamm') & (subm_df['Version']==forecast_select)]
 df
