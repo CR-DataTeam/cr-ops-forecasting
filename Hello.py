2f4e155ec9ee23cff0c76d694db2e418fb57002c
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-06-17 13:11:54 +0000

    sb

--- Hello.py
+++ Hello.py
@@ -44,7 +44,7 @@ st.write("# Forecasting Tool")
 
 col1, col2, col3 = st.columns([.25,1,.25])
 
-servline_list = ['Mamm','CIS','Vein']
+servline_list = ['','Mamm','CIS','Vein']
 forecast_list = ['Budget','01+11','02+10','03+09','04+08','05+07','06+06','07+05','08+04','09+03','10+02','11+01']
 fxarea_list   = ['Ops', 'Finance', 'Marketing', 'Other']
 with col2:
