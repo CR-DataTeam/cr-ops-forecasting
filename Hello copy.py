6139faec085ca76ad414b87b97b65465697ee505
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2024-07-27 03:49:06 +0000

    possible_cyclical

--- Hello copy.py
+++ Hello copy.py
@@ -21,17 +21,10 @@ from tempfile import NamedTemporaryFile
 
 LOGGER = get_logger(__name__)
 
-pg = st.navigation({
-    "Submit Files": [
-        st.Page('Hello.py', title="Submission Form", default=True, icon="📁"),
-        ],
-    "Review Forecasts": [
-        st.Page('pg01_Mamm.py', title="Mamm Forecast Files", icon="🏥"),
-        st.Page('pg02_CIS.py',  title="CIS Forecast Files",  icon="🏥"),
-        st.Page('pg03_Vein.py', title="Vein Forecast Files", icon="🏥"),
-        ],
-})
-pg.run()
+#st.set_page_config(
+#    page_title="Forecasting Tool",
+#     layout="wide"
+#)
 
 st.write("# Forecasting Tool")
 
